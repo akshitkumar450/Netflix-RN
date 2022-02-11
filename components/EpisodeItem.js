6fe85d27d4336ea5da9a905a1c28aa4c8454f19c
Akshit Kumar <akshitkumar450@gmail.com>
2022-02-11 23:28:11 +0530

    showing the episodes statically

---
+++ components/EpisodeItem.js
@@ -0,0 +1,36 @@
+import { View, Text, Image } from "react-native";
+import React from "react";
+import tw from "tailwind-react-native-classnames";
+import movie from "../assets/movie";
+import { AntDesign } from "@expo/vector-icons";
+const EpisodeItem = ({ movieItem }) => {
+  const episode = movie.seasons.items[0];
+  const firstEpisode = movieItem;
+  return (
+    <View style={tw`my-3`}>
+      <View style={tw`flex-row items-center justify-between mb-2`}>
+        <View style={tw`flex-1 flex-row items-center`}>
+          <Image
+            source={{ uri: movieItem.poster }}
+            style={{
+              width: 90,
+              height: 70,
+              resizeMode: "cover",
+              borderRadius: 10,
+            }}
+          />
+          <View style={tw`ml-2`}>
+            <Text style={tw`text-gray-400 `}>{movieItem.title}</Text>
+            <Text style={tw`text-gray-500 text-xs`}>{movieItem.duration}</Text>
+          </View>
+        </View>
+        <View>
+          <AntDesign name="download" size={24} color="white" />
+        </View>
+      </View>
+      <Text style={tw`text-gray-400`}>{movieItem.plot}</Text>
+    </View>
+  );
+};
+
+export default EpisodeItem;
--- screens/DetailsScreen.js
+++ screens/DetailsScreen.js
@@ -1,4 +1,11 @@
-import { View, Text, Image, Pressable, ScrollView } from "react-native";
+import {
+  View,
+  Text,
+  Image,
+  Pressable,
+  ScrollView,
+  FlatList,
+} from "react-native";
 import React from "react";
 import movie from "../assets/movie";
 import tw from "tailwind-react-native-classnames";
@@ -9,9 +16,10 @@ import {
   Ionicons,
   Feather,
 } from "@expo/vector-icons";
+import EpisodeItem from "../components/EpisodeItem";
 const DetailsScreen = () => {
-  const movieItem = movie.seasons.items[0].episodes.items[0];
-  console.log(movieItem);
+  const episodes = movie.seasons.items[0].episodes.items;
+  const movieItem = episodes[0];
   return (
     <View style={tw` flex-1`}>
       <Image
@@ -24,62 +32,67 @@ const DetailsScreen = () => {
           resizeMode: "cover",
         }}
       />
-      <ScrollView>
-        <View style={tw`p-2`}>
-          <Text style={tw`text-white text-3xl mt-1 font-semibold `}>
-            {movie.title}
-          </Text>
-          <View style={tw`flex-row`}>
-            <Text style={tw`text-green-500`}>98% match</Text>
-            <Text style={tw`text-gray-500 ml-4`}>{movie.year}</Text>
-            <View style={[tw`bg-yellow-400 rounded-md mx-2`, { padding: 2 }]}>
-              <Text style={tw`text-black`}>12+</Text>
-            </View>
-            <Text style={tw`text-gray-500 mr-4`}>
-              {movie.numberOfSeasons} seasons
+      <FlatList
+        data={episodes}
+        renderItem={({ item }) => <EpisodeItem movieItem={item} />}
+        //   it will be scrollable
+        ListHeaderComponent={
+          <View style={tw`p-2`}>
+            <Text style={tw`text-white text-3xl mt-1 font-semibold `}>
+              {movie.title}
             </Text>
-            <MaterialIcons name="hd" size={24} color="gray" />
-          </View>
-
-          <Pressable
-            style={tw`bg-gray-200 rounded-md my-2 h-10 justify-center`}>
-            <View style={tw`flex-row items-center justify-center`}>
-              <Entypo name="controller-play" size={24} color="black" />
-              <Text style={tw`text-lg font-semibold`}>Play</Text>
-            </View>
-          </Pressable>
-
-          <Pressable
-            style={tw`bg-gray-900 rounded-md mt-2 h-10 justify-center`}>
-            <View style={tw`flex-row items-center justify-center`}>
-              <AntDesign name="download" size={24} color="white" />
-              <Text style={tw`text-lg text-white font-semibold ml-2`}>
-                Download
+            <View style={tw`flex-row`}>
+              <Text style={tw`text-green-500`}>98% match</Text>
+              <Text style={tw`text-gray-500 ml-4`}>{movie.year}</Text>
+              <View style={[tw`bg-yellow-400 rounded-md mx-2`, { padding: 2 }]}>
+                <Text style={tw`text-black`}>12+</Text>
+              </View>
+              <Text style={tw`text-gray-500 mr-4`}>
+                {movie.numberOfSeasons} seasons
               </Text>
+              <MaterialIcons name="hd" size={24} color="gray" />
             </View>
-          </Pressable>
-          <Text style={tw`text-gray-300 my-2`}>{movie.plot}</Text>
-          <Text style={tw`text-gray-300`} numberOfLines={1}>
-            Cast : {movie.cast}
-          </Text>
-          <Text style={tw`text-gray-300`}>Created By : {movie.creator}</Text>
 
-          <View style={tw`flex-row justify-around items-center`}>
-            <View style={tw`items-center`}>
-              <AntDesign name="plus" size={28} color="white" />
-              <Text style={tw`text-gray-300 `}>My List</Text>
-            </View>
-            <View style={tw`items-center`}>
-              <Feather name="thumbs-up" size={28} color="white" />
-              <Text style={tw`text-gray-300`}>Rate</Text>
-            </View>
-            <View style={tw`items-center`}>
-              <Feather name="send" size={28} color="white" />
-              <Text style={tw`text-gray-300`}>Share</Text>
+            <Pressable
+              style={tw`bg-gray-200 rounded-md my-2 h-10 justify-center`}>
+              <View style={tw`flex-row items-center justify-center`}>
+                <Entypo name="controller-play" size={24} color="black" />
+                <Text style={tw`text-lg font-semibold`}>Play</Text>
+              </View>
+            </Pressable>
+
+            <Pressable
+              style={tw`bg-gray-900 rounded-md mt-2 h-10 justify-center`}>
+              <View style={tw`flex-row items-center justify-center`}>
+                <AntDesign name="download" size={24} color="white" />
+                <Text style={tw`text-lg text-white font-semibold ml-2`}>
+                  Download
+                </Text>
+              </View>
+            </Pressable>
+            <Text style={tw`text-gray-300 my-2`}>{movie.plot}</Text>
+            <Text style={tw`text-gray-300`} numberOfLines={1}>
+              Cast : {movie.cast}
+            </Text>
+            <Text style={tw`text-gray-300`}>Created By : {movie.creator}</Text>
+
+            <View style={tw`flex-row justify-around items-center`}>
+              <View style={tw`items-center`}>
+                <AntDesign name="plus" size={28} color="white" />
+                <Text style={tw`text-gray-300 `}>My List</Text>
+              </View>
+              <View style={tw`items-center`}>
+                <Feather name="thumbs-up" size={28} color="white" />
+                <Text style={tw`text-gray-300`}>Rate</Text>
+              </View>
+              <View style={tw`items-center`}>
+                <Feather name="send" size={28} color="white" />
+                <Text style={tw`text-gray-300`}>Share</Text>
+              </View>
             </View>
           </View>
-        </View>
-      </ScrollView>
+        }
+      />
     </View>
   );
 };
